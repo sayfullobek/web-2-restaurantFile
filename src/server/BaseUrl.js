9a3812d22b0cbbfb0198cbecb457709168f5b070
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-07-07 07:50:22 -0700

    Update BaseUrl.js

--- src/server/BaseUrl.js
+++ src/server/BaseUrl.js
@@ -1,1 +1,2 @@
-export const BaseUrl = "http://localhost:8080/api"
+// export const BaseUrl = "http://localhost:8080/api"
+export const BaseUrl = "https://restaran-web-2-23d3b13e4398.herokuapp.com/api"
